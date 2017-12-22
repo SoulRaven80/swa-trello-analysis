c2c01b978cc36ea59721d446baae386489e42efc
Leonardo Putz <leonardo.putz@globant.com>
2017-12-22 11:13:58 -0600

    Adding user name to welcome message after login

--- public/js/login.js
+++ public/js/login.js
@@ -21,3 +21,32 @@ var authenticationFailure = function(param) {
   console.log('Failed authentication');
 	processToken();
 };
+
+function getLoggedInfo(onSuccess, onError) {
+  window.Trello.rest('GET',
+    'members/me',
+    { token : localStorage.getItem('trello_token') },
+    onSuccess,
+		onError
+  );
+}
+
+function processToken() {
+  if (localStorage.getItem('trello_token') == undefined) {
+    $('#login_button').css('display', 'block');
+    $('#welcome_button').css('display', 'none');
+    $('#buttons_row').css('display', 'none');
+  }
+  else {
+    $('#login_button').css('display', 'none');
+    $('#welcome_button').css('display', 'block');
+    $('#buttons_row').css('display', 'block');
+  }
+  getLoggedInfo(
+    function(result) {
+      $('#welcome_button').text("Welcome " + result.fullName + "!");
+    },
+    function (result) {
+      console.log(result)
+    });
+}
