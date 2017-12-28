3b53c9990ad40ac0704fd3357c5b6dcbedac40be
Leonardo Putz <leonardo.putz@globant.com>
2017-12-28 14:35:37 -0600

    Added pie chart tooltips

--- public/js/pie.js
+++ public/js/pie.js
@@ -1,8 +1,9 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    radius = Math.min(width, height) / 2,
-    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var svg = d3.select("svg");
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+var radius = Math.min(width, height) / 2;
+var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var format = d3.format(",d");
 
 var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
@@ -30,7 +31,15 @@ d3.json(getJsonURLforAllCards("name,labels", false), function(error, cards) {
 
   arc.append("path")
       .attr("d", path)
-      .attr("fill", function(d) { return color(d.data.name); });
+      .attr("fill", function(d) { return color(d.data.name); })
+      .on("mouseover", function(d) {
+              tooltip.text(format(d.data.value));
+              tooltip.style("visibility", "visible");
+      })
+      .on("mousemove", function() {
+          return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+      })
+      .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
   // Get the angle on the arc and then rotate by -90 degrees
   var getAngle = function (d) {
@@ -44,4 +53,16 @@ d3.json(getJsonURLforAllCards("name,labels", false), function(error, cards) {
       ; })
       .attr("dy", "0.35em")
       .text(function(d) { return d.data.name; });
+
+      var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("color", "white")
+        .style("padding", "8px")
+        .style("background-color", "rgba(0, 0, 0, 0.75)")
+        .style("border-radius", "6px")
+        .style("font", "12px sans-serif")
+        .text("tooltip");
 });
